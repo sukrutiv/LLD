f7ad7b71eabafd9fe248028c06550d5ef97d6379
sukrutiv <sukrutipatel1992@gmail.com>
2024-10-30 16:13:37 -0400

    Breaking strategy one

---
+++ DesignPatterns/Singleton/SingletonBreak/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SingletonBreak;
+
+SingletonClone singleton = SingletonClone.Instance;
+SingletonClone clone = (SingletonClone)singleton.Clone();
+
+Console.WriteLine(singleton.GetHashCode());
+Console.WriteLine(clone.GetHashCode()); 
+
+Console.ReadLine();
---
+++ DesignPatterns/Singleton/SingletonBreak/SingletonClone.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonBreak
+{
+    public class SingletonClone : ICloneable
+    {
+
+        private static volatile SingletonClone instance;
+        private static object instanceLock = new object();
+        public static SingletonClone Instance
+        {
+            get
+            {
+                if (instance != null) return instance;
+                lock (instanceLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new SingletonClone();
+                    }
+                }
+                return instance;
+            }
+        }
+        private SingletonClone() { }    
+
+
+
+        public object Clone()
+        {
+            return (SingletonClone)base.MemberwiseClone();
+        }
+    }
+}
