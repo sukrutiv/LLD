a729eccc1811344fa9b433cdb1318eee50e33691
sukrutiv <sukrutipatel1992@gmail.com>
2024-10-30 15:52:47 -0400

    Update Singleton.cs

--- DesignPatterns/Singleton/Singleton.cs
+++ DesignPatterns/Singleton/Singleton.cs
@@ -1,6 +1,7 @@
 public sealed class Singleton
 {
     //the volatile keyword ensures that the instantiation is complete 
+    //before it can be accessed further helping with thread safety.
     private static volatile Singleton _instance;
     private static object _instanceLock = new object();
     public static Singleton Instance
